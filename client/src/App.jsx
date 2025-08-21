1bf2198202fa33db5aa4c28df1a20c1c306cc022
Melwin31 <melwin272005@gmail.com>
2025-08-21 21:41:42 +0530

    success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import GetStarted from "./components/GetStarted";
+import LevelSelection from "./components/LevelSelection";
+import Topics from "./components/Topics";
+import Questions from "./components/Questions";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<GetStarted />} />
+      <Route path="/levels" element={<LevelSelection />} />
+      <Route path="/topics/:level" element={<Topics />} />
+      <Route path="/questions/:level/:topicId" element={<Questions />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
